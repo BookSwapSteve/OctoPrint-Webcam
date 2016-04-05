70b1ff79fb01b9e8d0fe4c5635c82d80fe83be62
Stephen Harrison <Stephen.Harrison@AnalysisUK.com>
2016-04-05 11:53:31 +0100

    Update following the plugin tutorial

--- octoprint_webcam/__init__.py
+++ octoprint_webcam/__init__.py
@@ -4,9 +4,19 @@ from __future__ import absolute_import
 import octoprint.plugin
 
 class WebcamPlugin(octoprint.plugin.StartupPlugin,
-                       octoprint.plugin.TemplatePlugin):
+                       octoprint.plugin.TemplatePlugin,
+                       octoprint.plugin.SettingsPlugin):
     def on_after_startup(self):
-        self._logger.info("*** Webcam plugin on after startup ******")
+        #self._logger.info("*** Webcam plugin on after startup ******")
+		self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
+    
+	def get_settings_defaults(self):
+        return dict(url="https://en.wikipedia.org/wiki/Hello_world")
 
+    def get_template_configs(self):
+        return [
+            dict(type="navbar", custom_bindings=False),
+            dict(type="settings", custom_bindings=False)
+        ]
 __plugin_name__ = "Webcam"
 __plugin_implementation__ = WebcamPlugin()
